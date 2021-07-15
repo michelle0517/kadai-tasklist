0fa1e72e27d9b042e657d849ad104d6c8254b5ae
michelle0517 <maikooo.nluv@gmail.com>
2021-07-15 15:12:39 +0000

    login

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: 'toppages#index'
+  root to: 'tasks#index'
+#  resources :tasks, only: [:create, :destroy]
+  resources :tasks
+  
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+  
+  get 'signup', to: 'users#new'
+  resources :users, only: [:index, :show, :create]
+  
+  
+
 end
