616e150a23e3684db3bd296819c26dae56adfd1e
michelle0517 <maikooo.nluv@gmail.com>
2021-07-16 13:13:33 +0000

    revise

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,21 +1,22 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
   before_action :require_user_logged_in
   before_action :correct_user, only: [:destroy]
   
-  #before_action :set_task, only: [:show, :edit, :update, :destroy]
-  def index
-    @pagy, @tasks = pagy(Task.all, items:20)
-  end
   
   #def index
-    #if logged_in?
-      #@task = current_user.tasks.build  # form_with 用
-      #@pagy, @tasks = pagy(current_user.tasks.order(id: :desc))
-    #end
+    #@pagy, @tasks = pagy(Task.all, items:20)
   #end
   
-  #def show
-  #end
+  def index
+    if logged_in?
+      @task = current_user.tasks.build  # form_with 用
+      @pagy, @tasks = pagy(current_user.tasks.order(id: :desc))
+    end
+  end
+  
+  def show
+  end
   
   def new
     @task = Task.new
@@ -36,30 +37,30 @@ class TasksController < ApplicationController
     end
   end
   
-  #def edit
-  #end
+  def edit
+  end
   
-  #def update
-    #if @task.update(task_params)
-      #flash[:success] = 'Task は正常に更新されました'
-      #redirect_to @task
-    #else
-      #flash.now[:danger] = 'Task は更新されませんでした'
-      #render :edit
-    #end
-  #end
+  def update
+    if @task.update(task_params)
+      flash[:success] = 'Task は正常に更新されました'
+      redirect_to @task
+    else
+      flash.now[:danger] = 'Task は更新されませんでした'
+      render :edit
+    end
+  end
   
   def destroy
     @task.destroy
     flash[:success] = 'Task は正常に削除されました'
-    redirect_back(fallback_location: root_path)
+    redirect_to tasks_url
   end
   
   private
   
-  #def set_task
-    #@task = Task.find(params[:id])
-  #end
+  def set_task
+    @task = Task.find(params[:id])
+  end
 
   # Strong Parameter
   def task_params
